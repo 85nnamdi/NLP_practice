4d3e887e5f5783f346305ec7b65b983acefba3a2
Nnamdi Ekweariri <ekwearirinnamdi@gmail.com>
2021-04-21 13:11:10 +0200

    write generated text to file

--- blogGeneration.py
+++ blogGeneration.py
@@ -1,8 +1,8 @@
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from transformers import GPT2Tokenizer, GPT2Model
 
 #load model
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2-large')
-model = GPT2LMHeadModel.from_pretrained('gpt2-large',pad_token_id=tokenizer.eos_token_id)
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2-medium')
+model = GPT2Model.from_pretrained('gpt2-medium',pad_token_id=tokenizer.eos_token_id)
 
 #tokenization step
 sentence = "Aristole was a great philosopher!"
@@ -10,3 +10,11 @@ input_id = tokenizer.encode(sentence, return_tensors='pt')
 
 input_id
 # tokenizer.decode(input_id[0])
+
+#generate and decode text 
+output_text = model.generate(input_id, max_length=100, num_beams=5, no_repeat_ngram_size=2, early_stopping=True)
+
+#write output to text file
+text = tokenizer.decode(output_text[0], skip_special_tokens=True)
+with open('GeneratedText.txt', 'w') as f:
+    f.write(text)
